cd96e4aaa4f504d12f60ae1cfed5371165f5ce2b
erikbenton <erik.benton@hotmail.com>
2018-03-24 21:51:16 -0400

    finishing very basic version of game

--- app/src/main/java/com/example/android/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/android/colorpicker/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.android.colorpicker;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     int[] winningColor = new int[3];
+    String winningColorString;
     int red;
     int green;
     int blue;
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     int min = 0;
 
     TextView title;
+
+    LinearLayout background;
 
     String button1Color;
     Button button1;
@@ -34,10 +41,13 @@ public class MainActivity extends AppCompatActivity {
     String button5Color;
     Button button5;
 
+    Button resetButton;
+
     String button6Color;
     Button button6;
 
-    Button[] buttons = new Button[6]; //{button1, button2, button3, button4, button5, button6};
+    String[] buttonColors = new String[6];
+    Button[] buttons = new Button[6];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,20 +56,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
-        setTitle();
-        setButtonColors();
+        initGameScreen();
 
     }
     public void initViews()
     {
         title = findViewById(R.id.title);
 
-        button1 = findViewById(R.id.button_1);
-        button2 = findViewById(R.id.button_2);
-        button3 = findViewById(R.id.button_3);
-        button4 = findViewById(R.id.button_4);
-        button5 = findViewById(R.id.button_5);
-        button6 = findViewById(R.id.button_6);
+        background = findViewById(R.id.background);
+
+        button1      = findViewById(R.id.button_1);
+        button2      = findViewById(R.id.button_2);
+        button3      = findViewById(R.id.button_3);
+        button4      = findViewById(R.id.button_4);
+        button5      = findViewById(R.id.button_5);
+        button6      = findViewById(R.id.button_6);
+        resetButton  = findViewById(R.id.reset_button);
 
         buttons[0] = button1;
         buttons[1] = button2;
@@ -67,6 +79,14 @@ public class MainActivity extends AppCompatActivity {
         buttons[3] = button4;
         buttons[4] = button5;
         buttons[5] = button6;
+    }
+
+    public void initGameScreen()
+    {
+        background.setBackgroundColor(Color.rgb(0, 0, 0));
+        resetButton.setText("Reset");
+        setTitle();
+        setButtonColors();
     }
 
     public void setTitle()
@@ -80,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
         winningColor[0] = red;
         winningColor[1] = green;
         winningColor[2] = blue;
+        winningColorString = "" + winningColor[0] + winningColor[1] + winningColor[2];
 
         titleText += red + ", " + green + ", " + blue + ")";
 
@@ -88,12 +109,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void setButtonColors()
     {
+        int redTemp;
+        int greenTemp;
+        int blueTemp;
+
         for(int i = 0; i < buttons.length; i++)
         {
+            redTemp = getRandomInt();
+            greenTemp = getRandomInt();
+            blueTemp = getRandomInt();
+            buttons[i].setVisibility(View.VISIBLE);
             buttons[i].setBackgroundColor(Color.rgb(getRandomInt(), getRandomInt(), getRandomInt()));
+            buttonColors[i] = "" + redTemp + greenTemp + blueTemp;
         }
 
-        buttons[(int)(Math.random() * 6)].setBackgroundColor(Color.rgb(winningColor[0], winningColor[1], winningColor[2]));
+        int randomSquare = (int)(Math.random() * 6);
+        buttons[randomSquare].setBackgroundColor(Color.rgb(winningColor[0], winningColor[1], winningColor[2]));
+        buttonColors[randomSquare] = "" + winningColor[0] + winningColor[1] + winningColor[2];
     }
 
     public String convertRGBToHex(int red, int green, int blue)
@@ -106,10 +138,87 @@ public class MainActivity extends AppCompatActivity {
         return (int)(Math.random() * max + min);
     }
 
+    public void winGame()
+    {
+        title.setText("That was it!");
+        background.setBackgroundColor(Color.rgb(winningColor[0], winningColor[1], winningColor[2]));
+        resetButton.setText("Play Again?");
+    }
+
     public void resetGame(View view)
     {
-        initViews();
-        setTitle();
-        setButtonColors();
+        initGameScreen();
+    }
+
+    public void button1Press(View view)
+    {
+        if(buttonColors[0].equals(winningColorString))
+        {
+            winGame();
+        }
+        else
+        {
+            button1.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void button2Press(View view)
+    {
+        if(buttonColors[1].equals(winningColorString))
+        {
+            winGame();
+        }
+        else
+        {
+            button2.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void button3Press(View view)
+    {
+        if(buttonColors[2].equals(winningColorString))
+        {
+            winGame();
+        }
+        else
+        {
+            button3.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void button4Press(View view)
+    {
+        if(buttonColors[3].equals(winningColorString))
+        {
+            winGame();
+        }
+        else
+        {
+            button4.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void button5Press(View view)
+    {
+        if(buttonColors[4].equals(winningColorString))
+        {
+            winGame();
+        }
+        else
+        {
+            button5.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void button6Press(View view)
+    {
+        if(buttonColors[5].equals(winningColorString))
+        {
+            winGame();
+        }
+        else
+        {
+            button6.setVisibility(View.INVISIBLE);
+        }
     }
 }
